50b8f3dbf5846b80738db6964872494992dd531d
LoafOfCode <53025614+LoafOfCode@users.noreply.github.com>
2021-09-07 18:08:48 -0700

    added remove button and local cache

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
 import getHex from './Tools/GetHex';
 import shortid from 'shortid';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './Styles/App.css';
 
 function App() {
   let [colors, setColors] = useState<string[]>([]);
+  let LOCAL_STORE = 'randoColors.cachedColors';
 
   let hex = () => {
-    let __FINDHEX__ = getHex();
     setColors (
       [
         ...colors,
-        __FINDHEX__
+        getHex()
       ]
     )
   }
-  
+  useEffect(() => {
+    const colors__CACHE = JSON.parse(localStorage.getItem(LOCAL_STORE) || '{}');
+    if(colors__CACHE) setColors(colors__CACHE)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORE, JSON.stringify(colors))
+  }, [colors])
+  /*
+  Local Storage caching basically stolen from WDS, 
+  thanks Kyle
+  */
   return (
     <div className="wrapper">
       <button className="generate-button" onClick = {hex}>Generate A New Color</button>
+      <button className="remove-button" onClick = {
+        () => setColors([])
+      }>Remove Colors</button>
       {
         colors.map((item) => 
           <h3 className="color" style={{backgroundColor: `#${item}`}} key={shortid.generate()}>
